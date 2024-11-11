11674f5cf8f7d59c33441040a45ced2d82dbff28
Mikaela-D <G00401392@atu.ie>
2024-11-11 17:24:03 +0000

    Added new project microservice customer

---
+++ src/main/java/ie/atu/ecommerceteamprojectcustomer/ECommerceTeamProjectCustomerApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.ecommerceteamprojectcustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceTeamProjectCustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceTeamProjectCustomerApplication.class, args);
+    }
+
+}
