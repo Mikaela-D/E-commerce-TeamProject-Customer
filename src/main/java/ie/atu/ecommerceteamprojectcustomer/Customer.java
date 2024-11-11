16fbac3d4b528f5b0dc5b67219c3306bda5c1f3b
Mikaela-D <G00401392@atu.ie>
2024-11-11 18:21:31 +0000

    Added regex to Customer

--- src/main/java/ie/atu/ecommerceteamprojectcustomer/Customer.java
+++ src/main/java/ie/atu/ecommerceteamprojectcustomer/Customer.java
@@ -3,6 +3,7 @@ package ie.atu.ecommerceteamprojectcustomer;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,6 +23,7 @@ public class Customer {
 
     @NotBlank(message = "Name is required")
     @Size(min = 2, max = 30, message = "Name must be between 2 and 30 characters")
+    @Pattern(regexp = "^[A-Za-z ]+$", message = "Name can only have letters and spaces")
     private String name;
 
     @NotBlank(message = "Email is required")
@@ -30,5 +32,6 @@ public class Customer {
 
     @NotBlank(message = "Address is required")
     @Size(max = 80, message = "Address must be less than 80 characters")
+    @Pattern(regexp = "^[A-Za-z0-9,\\s]+$", message = "Address can only have letters, numbers, commas, and spaces")
     private String address;
 }
