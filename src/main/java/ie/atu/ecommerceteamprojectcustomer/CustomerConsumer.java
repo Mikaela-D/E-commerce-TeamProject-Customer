ca804e03bca7415a9b5f362f9823fd5ea5c1216b
par2hibATU <parthibbdcox@gmail.com>
2025-01-06 15:49:07 +0000

    new configs for the RabbitMQ

--- src/main/java/ie/atu/ecommerceteamprojectcustomer/CustomerConsumer.java
+++ src/main/java/ie/atu/ecommerceteamprojectcustomer/CustomerConsumer.java
@@ -3,13 +3,19 @@ package ie.atu.ecommerceteamprojectcustomer;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
 @ConditionalOnProperty(name = "consumer.enabled", havingValue = "true")
 public class CustomerConsumer {
-    @RabbitListener(queues = "customerQueue")
-    public void processProductMessage(Customer customer) {
-        System.out.println("Received message: " + customer.toString());
+    @RabbitListener(queues = RabbitMQConfig.QUEUE)
+    public void processProductMessage(Product product) {
+        System.out.println("Received message: " + product.toString());
+    }
+
+    @RabbitListener(queues = RabbitMQConfig.PAYMENT_QUEUE)
+    public void processProductMessage(Payment payment) {
+        System.out.println("Received message: " + payment.toString());
     }
 }
